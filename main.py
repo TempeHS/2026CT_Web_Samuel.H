efd3522eb59ff47bf56ffcd6c5d0da89d2cdd282
Samuel H <samuel.haszard@education.nsw.gov.au>
2025-05-27 23:39:47 +0000

    Cards

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     card_data = (
-        ("David", "We are all about teaching, and passing on our information to others so that people can create better videos, faster and at a low cost", "Find Out More", "static/images/card1"),
-        ("David", "Boost your editing speed with simple techniques! Learn shortcuts, key tools, and smooth workflows to refine text and visuals efficiently. Let’s get started! ", "Find Out More", "static/images/card2"),
-        ("David", "Enhance precision and speed! Master advanced shortcuts, refine techniques, and optimize workflows for faster, high-quality edits. Let’s go!", "Find Out More", "static/images/card3"),
-        ("David", "Master expert techniques, optimize workflows, and edit with unmatched speed and precision. Take your skills to the highest level!", "Find Out More", "static/images/card4"),
-        ("David", "Some Text", "Find Out More", "static/images/card5"),
-        ("David", "Some Text", "Find Out More", "static/images/card6"),
+        ("", "We are all about teaching, and passing on our information to others so that people can create better videos, faster and at a low costㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ", "Find Out More", "static/images/card1.png"),
+        ("", "Boost your editing speed with simple techniques! Learn shortcuts, key tools, and smooth workflows to refine text and visuals efficiently. Let’s get started!ㅤㅤㅤㅤㅤㅤ", "Find Out More", "static/images/card2.png"),
+        ("", "Enhance precision and speed! Master advanced shortcuts, refine techniques, and optimize workflows for faster, high-quality edits. Let’s go!ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ", "Find Out More", "static/images/card3.png"),
+        ("", "Master expert techniques, optimize workflows, and edit with unmatched speed and precision. Take your skills to the highest level!ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ", "Find Out More", "static/images/card4.png"),
+        ("", "Video editing is about refining footage efficiently, using smart techniques to speed up trimming, transitions, and enhancements—maximizing quality while saving time.", "Find Out More", "static/images/card5.png"),
+        ("", "Our Goal is to optimize editing speeds to save time while keeping quality high—smart techniques make the process faster and more efficient.ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ", "Find Out More", "static/images/card6.png"),
     )
-    return render_template("index.html"), 200
+    return render_template("index.html", cards=card_data), 200
 
 
 if __name__ == '__main__':
